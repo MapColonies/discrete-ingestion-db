990bbe63e9bcf1cfa68f1ecf035e18d90a345f1b
Shahar s <cl_shaharshavit@rnd-hub.com>
2021-03-22 11:07:34 +0200

    chore: lint

--- src/common/utils/convertTaskEntityToResponse.ts
+++ src/common/utils/convertTaskEntityToResponse.ts
@@ -2,6 +2,7 @@ import { PartialTaskEntity } from '../../DAL/entity/partialTask';
 import { IPartialTaskResponse } from '../interfaces';
 
 export default function convert(task: PartialTaskEntity): IPartialTaskResponse {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { discrete, ...taskResponse } = task;
   const response: IPartialTaskResponse = taskResponse;
   return response;
