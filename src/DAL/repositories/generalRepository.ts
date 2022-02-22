2017215fa0e47754a37d45a697e41065c72a7dd6
Roee Shachar <shacharroy14@gmail.com>
2022-02-22 15:14:05 +0200

    fix: specific query doesnt support shcema (#59)
    * fix: specific query doesnt support shcema
    
    * fix: query
    
    * fix: lint

--- src/DAL/repositories/generalRepository.ts
+++ src/DAL/repositories/generalRepository.ts
@@ -4,16 +4,18 @@ import { Services } from '../../common/constants';
 import { IConfig, IDbConfig } from '../../common/interfaces';
 
 export class GeneralRepository<T> extends Repository<T> {
+  protected readonly dbConfig: IDbConfig;
   private readonly config: IConfig;
+
   public constructor() {
     super();
     this.config = container.resolve(Services.CONFIG);
+    this.dbConfig = this.config.get('typeOrm');
   }
 
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   public async query(query: string, parameters?: any[] | undefined): Promise<any> {
-    const { schema } = this.config.get<IDbConfig>('typeOrm');
-    await super.query(`SET search_path TO "${schema as string}", public`);
+    await super.query(`SET search_path TO "${this.dbConfig.schema as string}", public`);
     return super.query(query, parameters);
   }
 }
